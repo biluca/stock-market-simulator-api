2a03896a723fe33fd8a5d69cb92dc3aeacc12d4b
biluca <vinicios.biluca@loadsmart.com>
2023-08-14 22:53:54 -0300

    Adding Initial Auto Migrations

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,34 @@ from django.contrib import admin
 
 from api.models import Owner, Stock, Portfolio, PriceMovement
 
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = [
+        "abbreviation",
+        "company_name",
+        "description",
+        "industry",
+        "stock_market",
+    ]
+
+    list_filter = [
+        "abbreviation",
+        "company_name",
+    ]
+
+
+@admin.register(PriceMovement)
+class PriceMovementAdmin(admin.ModelAdmin):
+    list_display = [
+        "stock_abbreviation",
+        "price",
+        "movement_amount",
+        "percentual_change_friendly",
+        "created_at",
+        "updated_at",
+    ]
+
+
 admin.site.register(Owner)
-admin.site.register(Stock)
 admin.site.register(Portfolio)
-admin.site.register(PriceMovement)
